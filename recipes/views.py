ed660d74a0b2cc4646681c3a32c7a0f47473a189
diogo-ribicki <diogo_ribicki@edu.univali.br>
2026-01-21 22:20:04 -0300

    Refactor home page: move head content to a separate partial and update home view path

--- recipes/views.py
+++ recipes/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={ 'name' : 'Diogo', 
+    return render(request, 'recipes/pages/home.html', context={ 'name' : 'Diogo', 
                                                          })
